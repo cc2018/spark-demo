1c42c916db6ab610929a815f86fb72cd9af629aa
caojian02 <caojian02@baidu.com>
2017-11-26 22:53:23 +0800

    hive sql

--- src/main/scala/cc/examples/WordCount.scala
+++ src/main/scala/cc/examples/WordCount.scala
@@ -5,15 +5,18 @@ import org.apache.spark.SparkContext
 
 object WordCount {
   def main(args: Array[String]) {
+    var file = ""
     if (args.length < 1) {
-      System.err.println("Usage: <file>")
-      System.exit(1)
+      System.err.println("No input file, use default hdfs file for test")
+      file = "hdfs://localhost:9100/test/hellospark"
+    } else {
+      file = args(0)
     }
 
     // hdfs://localhost:9100/test/hellospark
     val conf = new SparkConf().setAppName("WordCount")
     val sc = new SparkContext(conf)
-    val line = sc.textFile(args(0))
+    val line = sc.textFile(file)
 
     line.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _).collect().foreach(println)
     sc.stop()
